9e0c1fe9b0fa99f7ce989b88f7ddd9467f57b176
hkwany <380868513@qq.com>
2021-03-26 09:16:36 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # deploy all (challenge_servers + monitor_server)
-ansible-playbook -i hosts deploy.yml -v --skip-tags "update"
+# ansible-playbook -i inventory deploy.yml -v 
 
-# comment already set challenge_servers in hosts and then deploy the new one(s) 
-# any one of two options below is fine
-# ansible-playbook -i hosts deploy.yml -v --tags "challenge_servers"
-# ansible-playbook -i hosts deploy.yml -v --skip-tags "monitor_server"
-# ansible-playbook -i hosts deploy.yml -v --tags "docker"
+# deploy ELK Server only
+# ansible-playbook -i inventory deploy.yml -v --tags "ELK_Server"
 
-# update
-# ansible-playbook -i hosts deploy.yml -v --tags "update"
+# deploy ELK Client only
+# ansible-playbook -i inventory deploy.yml -v --tags "ELK_Client"
+
+# skip deploy ELK Server (same as upper one)
+# ansible-playbook -i inventory deploy.yml -v --skip-tags "ELK_Server"
