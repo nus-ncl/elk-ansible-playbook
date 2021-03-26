ab20f7c6f1bddc5181d25e90e2ab6011d6856aa6
hkwany <hkwany520@gmail.com>
2021-03-26 09:12:47 +0800

    second commit

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # deploy all (challenge_servers + monitor_server)
-ansible-playbook -i hosts deploy.yml -v --skip-tags "update"
+ansible-playbook -i inventory deploy.yml -v --tags "ELK_Client"
+ #ansible-playbook -i inventory deploy.yml -v --tags "Update_ELK_Server"
 
-# comment already set challenge_servers in hosts and then deploy the new one(s) 
+# comment already set challenge_servers in inventory and then deploy the new one(s) 
 # any one of two options below is fine
-# ansible-playbook -i hosts deploy.yml -v --tags "challenge_servers"
-# ansible-playbook -i hosts deploy.yml -v --skip-tags "monitor_server"
-# ansible-playbook -i hosts deploy.yml -v --tags "docker"
+# ansible-playbook -i inventory deploy.yml -v --tags "challenge_servers"
+# ansible-playbook -i inventory deploy.yml -v --skip-tags "monitor_server"
+# ansible-playbook -i inventory deploy.yml -v --tags "docker"
 
 # update
-# ansible-playbook -i hosts deploy.yml -v --tags "update"
+# ansible-playbook -i inventory deploy.yml -v --tags "update"
